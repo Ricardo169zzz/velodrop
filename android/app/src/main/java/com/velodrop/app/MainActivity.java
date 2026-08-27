8612327025afa76af0bc29243516d87f8d2c5274
Ricardo169zzz <miniajjaa11@gmail.com>
2026-08-27 20:12:56 +0700

    feat: add official dark app launcher icons and enforce dark Android navigation bar

--- android/app/src/main/java/com/velodrop/app/MainActivity.java
+++ android/app/src/main/java/com/velodrop/app/MainActivity.java
@@ -1,5 +1,30 @@
 package com.velodrop.app;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Window window = getWindow();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#08090C"));
+            window.setNavigationBarColor(Color.parseColor("#08090C"));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            View decorView = window.getDecorView();
+            int flags = decorView.getSystemUiVisibility();
+            // Clear light flags so buttons are light/crisp against the dark background
+            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+            flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            decorView.setSystemUiVisibility(flags);
+        }
+    }
+}
